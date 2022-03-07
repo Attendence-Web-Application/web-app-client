fe08ab6270a2f625b0cef48a28122ea62156d70e
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2022-03-07 11:30:48 -0800

    Add: add 404 page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,13 +1,51 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import { Container } from 'bootstrap-4-react';
+import errorImage from '../assets/404.png'
 const ErrorPage = () => {
-  return <h1>404</h1>
+  return (
+    <Wrapper>
+      <Container class="container col-sm-10 col-sm-offset-1">
+        <div>
+          <h1>Oh No! Error 404</h1>
+          <p>The page you are looking for not avaible!</p>
+        </div>
+        <Link to="/" className="back__home">Go to Home</Link>
+      </Container>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.main`
-  h1 {
-    font-size: 10rem;
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
+  background: url(${errorImage}) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  height:100vh;
+  font-family: 'Poppins', sans-serif;
+
+  .back__home{
+      color: #fff !important;
+      padding: 10px 20px;
+      background: hsl(230, 69%, 61%);;
+      margin: 20px 0;
+      border-radius:20px;
+      display: inline-block;
+      text-decoration: none!important;
+      transition: 0.5s;
+  }
+
+  .back__home:hover{
+      background-color: hsl(230, 57%, 53%) !important;
+  }
+  h1{
+    padding-top:10%;
+  }
+  p{
+      color: grey;
   }
 `
 
