24182d2b93479b9ac813bee6a35df21fdd2ca421
cst950928 <cst950928@hotmail.com>
2022-04-24 13:28:16 -0700

    Temperary storage

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'http://localhost:8888'
 
 // LoginComponent.js
 export const LOGIN_CHECK_API = BASE_URL + '/login'
@@ -23,15 +23,15 @@ export const CREATE_USER_URL = BASE_URL + '/user/createUser'
 
 // UserHomePage.js
 export const FIND_CLASS_ID_URL =
-  BASE_URL + '/class_enrolled/getClassEnroll/user'
+  BASE_URL + '/classEnrolled/getClassEnroll/user'
 export const FIND_CLASS_URL = BASE_URL + '/class/getClass/id'
 export const FIND_CLASS_BY_USER_URL = BASE_URL + '/class/userId/'
 
 // AddClassroomProfessor.js， AddClassroomStudent.js
 export const GET_ALL_CLASS_URL = BASE_URL + '/class/getClass/'
 export const CREATE_CLASS_URL = BASE_URL + '/class/createClass'
-export const INSERT_ENROLL_URL = BASE_URL + '/class_enrolled/createEnroll/'
-export const CHECK_ENROLL_URL = BASE_URL + '/class_enrolled/getClassEnroll/'
+export const INSERT_ENROLL_URL = BASE_URL + '/classEnrolled/createEnroll/'
+export const CHECK_ENROLL_URL = BASE_URL + '/classEnrolled/getClassEnroll/'
 export const SEARCH_CLASS_URL = BASE_URL + '/class/getClassByTitle/'
 
 // AttendanceDetailBySession.js
@@ -45,7 +45,7 @@ export const FIND_ATTENDANCE_STATUS_URL = BASE_URL + '/attendanceRecord/user/'
 
 // AttendenceRecordTable.js
 export const FIND_STUDENT_ID_URL =
-  BASE_URL + '/class_enrolled/getClassEnroll/user'
+  BASE_URL + '/classEnrolled/getClassEnroll/user'
 export const FIND_ROLL_CALL_ID_URL = BASE_URL + '/attendanceRecord/user/'
 export const FIND_ROLL_CALL_URL = BASE_URL + '/rollCall/'
 export const FIND_ALL_USER_BY_ROLLCALL_ID =
@@ -54,18 +54,18 @@ export const FIND_STUDENT = BASE_URL + '/user/'
 
 // Classrooms.js
 export const DELETE_STUDENT_CLASS_URL =
-  BASE_URL + '/class_enrolled/getClassEnroll/'
+  BASE_URL + '/classEnrolled/getClassEnroll/'
 export const DELETE_PROFESSOR_CLASS_URL = BASE_URL + '/class/getClass/Id'
 
 // RollCall.js
 export const FIND_ALL_ENROLL_STUDENT_URL =
-  BASE_URL + '/class_enrolled/getUser/class'
+  BASE_URL + '/classEnrolled/getUser/class'
 export const CREATE_ROLL_CALL_URL = BASE_URL + '/rollCall/createRollCall'
 
 // StudentAttendanceTable.js
 export const FIND_ALL_USER = BASE_URL + '/user/nameIdPair/'
 export const FIND_ALL_STUDENT_BY_CLASS_ID =
-  BASE_URL + '/class_enrolled/getClassEnroll/class'
+  BASE_URL + '/classEnrolled/getClassEnroll/class'
 
 //InvitationForm.js
 export const GET_ALL_TITLE = BASE_URL + '/class/getDistinctClassTitle'
